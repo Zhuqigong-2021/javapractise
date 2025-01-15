4cc98ae6eff0ff2b22912425b73de509a95a7aa4
philzhuqigong <philzhuqigong@gmail.com>
2025-01-14 21:38:29 -0500

    pseudo code for handling exception

---
+++ src/main/java/trycatch/Exception.java
@@ -0,0 +1,22 @@
+package trycatch;
+
+public class Exception {
+//    异常机制
+//    try{
+//        open the file;
+//        determine its size;
+//        allocate that much memory;
+//        read the file into memory;
+//        close the file;
+//    }catch(fileOpenFailed){
+//        doSomething
+//    }catch(sizeDeterminationFailed){
+//        doSomething
+//    }catch(memoryAllocationFailed){
+//        doSomething
+//    }catch(readFailed){
+//        doSomething
+//    }catch(fileCloseFailed){
+//        doSomething
+//    }
+}
