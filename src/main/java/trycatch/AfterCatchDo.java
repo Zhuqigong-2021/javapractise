00099bb1dd9b4149a2abef2c4e12629490188a2d
philzhuqigong <philzhuqigong@gmail.com>
2025-01-14 21:29:24 -0500

    throw,e.getmessage(),e,e.printStackTrace()

---
+++ src/main/java/trycatch/AfterCatchDo.java
@@ -0,0 +1,43 @@
+package trycatch;
+
+public class AfterCatchDo {
+    public static void f(){
+        int[] a = new int[10];
+        a[10]=10;
+        System.out.println("Hello");
+    }
+    public static  void g(){
+        f();
+    }
+
+    public static void h(){
+        int i=10;
+        if(i<100){
+            g();
+        }
+    }
+
+    public static void k(){
+        try {
+            h();
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("k()");
+//            throw e;
+        }
+    }
+    public static void main(String[] args) {
+
+        try {
+            k();
+
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Caught");
+            System.out.println(e.getMessage());
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        System.out.println("main");
+
+    }
+}
